58776f62ccd7383b7b42ca61e1c6509c22de02fc
josfire18 <jarockwood14@my.trine.edu>
2017-04-20 14:03:05 -0400

    More Main Menu work

--- SpaceInvaders/mainmenu.cpp
+++ SpaceInvaders/mainmenu.cpp
@@ -3,6 +3,8 @@
 #include "gamewindow.h"
 #include <QDir>
 #include <QCoreApplication>
+#include <QDebug>
+#include <QDesktopWidget>
 
 MainMenu::MainMenu(QWidget *parent) :
     QMainWindow(parent),
@@ -19,23 +21,25 @@ MainMenu::MainMenu(QWidget *parent) :
 #endif
 
     //Lock window size
-    this->setFixedSize(1000,500);
-    ui->cmdStartGame->move(400,419);
+    //this->setMaximumSize();
+    QRect rec = QApplication::desktop()->screenGeometry();
+    int height = rec.height();
+    int width = rec.width();
+    qDebug() << height << ", " << width;
+    this->resize(width, height);
 
     //Set Main Menu Background
     QPixmap bkgnd("./images/MainMenu.jpg");
-    bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
+    bkgnd = bkgnd.scaled(this->size(), Qt::KeepAspectRatio);
     QPalette palette;
     palette.setBrush(QPalette::Background, bkgnd);
     this->setPalette(palette);
 
+    ui->cmdStartGame->move(800,819);
+
     //Set transparent Start button bg
     //ui->cmdStartGame->setStyleSheet("background-color: rgba(255, 255, 255, 0);");
     ui->cmdStartGame->setStyleSheet("background-color: black; color: white;");
-
-
-
-
 }
 
 MainMenu::~MainMenu()
